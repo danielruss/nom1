4b56e54c1b8b122f61c8b0931a9fdb5c78bf6099
Daniel Russ <druss@mail.nih.gov>
2025-02-18 13:10:44 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+#[allow(unused_imports)]
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take_until;
-use nom::character::complete::multispace0;
+use nom::bytes::complete::take_while;
+use nom::character::complete::multispace1;
 use nom::character::complete::space0;
-use nom::multi::many0;
+use nom::combinator::opt;
+use nom::multi::{many0, many1};
 use nom::sequence::delimited;
 use nom::sequence::terminated;
 use nom::sequence::tuple;
@@ -99,31 +102,43 @@ impl Tag {
 }
 
 fn take_until_next_module_item(input: &str) -> IResult<&str, &str> {
-    let chars = input.char_indices();
+    let mut chars = input.char_indices();
     let mut char2 = input.chars();
     let _ = char2.next();
 
-    for ci in chars {
+    while let Some((current_index, current_char)) = chars.next() {
         let optional_next_char = char2.next();
-        if let Some(next_char) = optional_next_char {
-            if ci.1 == '[' && next_char.is_uppercase() {
-                //println!("--- found {}{:?}", ci.1, next_char);
-                return Ok((&input[ci.0..], &input[0..ci.0]));
+        match (current_char, optional_next_char) {
+            // there is a comment scan until \n or EOS...
+            ('/', Some('/')) => {
+                while let Some((_, in_comment_char)) = chars.next() {
+                    let _ = char2.next();
+                    if in_comment_char == '\n' {
+                        break;
+                    }
+                }
             }
-            if ci.1 == '<' {
+            // hit new question?
+            ('[', Some(next_char)) if next_char.is_uppercase() => {
+                return Ok((&input[current_index..], &input[0..current_index]));
+            }
+            // hit grid/loop?
+            ('<', Some(_)) => {
                 // safely calculate start and end to make sure that
                 // we dont hit any multi-byte characters.  If we hit a multibyte
                 // character, then it is not the end of the module item.
-                let start = (ci.0 + 1).min(input.len());
-                let end = (ci.0 + 5).min(input.len());
+                let start = (current_index + 1).min(input.len());
+                let end = (current_index + 5).min(input.len());
                 if input.is_char_boundary(end) && end > start {
                     let tag = &input[start..end];
                     if tag == "loop" || tag == "grid" {
                         //println!("--- found tag {}", tag);
-                        return Ok((&input[ci.0..], &input[0..ci.0]));
+                        return Ok((&input[current_index..], &input[0..current_index]));
                     }
                 }
             }
+            // keep going
+            (_, _) => {}
         }
     }
     Ok(("", input))
@@ -153,19 +168,44 @@ fn parse_tag(input: &str) -> IResult<&str, Tag> {
 fn parse_loop(input: &str) -> IResult<&str, (&str, Vec<ModuleItem>)> {
     let (input, markdown) = terminated(take_until("</loop>"), tag("</loop>"))(input)?;
     let loop_input = markdown;
+    println!("in parse_loop: ======= \n loop input:\n{:?}", loop_input);
     let (_, items) = many0(parse_question_loop_grid)(loop_input)?;
+    println!("items:\n {:?}", items);
 
-    return Ok((input, (markdown, items)));
+    Ok((input, (markdown, items)))
 }
 
 fn parse_grid(input: &str) -> IResult<&str, &str> {
     let (input, markdown) = terminated(take_until("</grid>"), tag("</grid>"))(input)?;
-    return Ok((input, markdown));
+    Ok((input, markdown))
+}
+
+fn comment(input: &str) -> IResult<&str, &str> {
+    println!("==============================>{}<===============", input);
+    let (input, (_, comment, _)) =
+        tuple((tag("//"), take_while(|c| c != '\n'), opt(tag("\n"))))(input)?;
+    Ok((input, comment))
+}
+
+fn parse_whitespace_or_comment(input: &str) -> IResult<&str, &str> {
+    println!("Parsing input: '{}'", input);
+    //let (input, _) = many0(alt((multispace0, comment)))(input)?;
+    let (input, _) = many0(alt((comment, multispace1)))(input)?;
+    println!("Remaining input: '{}'", input);
+    Ok((input, ""))
 }
 
 fn parse_question_loop_grid(input: &str) -> IResult<&str, ModuleItem> {
     // nom nom nom any whitespace..
-    let (input, _) = multispace0(input)?;
+    println!(
+        "in parse_question_loop_grid: before nomming off ws...\n{}",
+        input
+    );
+    let (input, _) = parse_whitespace_or_comment(input)?;
+    println!(
+        "in parse_question_loop_grid: after nomming off ws...\n{}",
+        input
+    );
     alt((parse_question, parse_loop_grid))(input)
 }
 
@@ -177,6 +217,7 @@ fn parse_loop_grid(input: &str) -> IResult<&str, ModuleItem> {
             Ok((input, ModuleItem::new_grid(tag, markdown)))
         }
         "loop" => {
+            println!("in parse_loop_grid... (loop) \n{}", input);
             let (input, (markdown, questions)) = parse_loop(input)?;
             Ok((input, ModuleItem::new_loop(tag, markdown, questions)))
         }
@@ -198,6 +239,9 @@ pub fn parse_module(input: &str) -> IResult<&str, Module> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[allow(unused_imports)]
+    use nom::{character::complete::alpha0, error::Error};
+    use nom::{character::complete::multispace0, sequence::preceded, Err};
     use regex::Regex;
 
     #[test]
@@ -273,7 +317,7 @@ mod tests {
                     Tag::new("loop", ""),
                     "[Q1]lala\n[Q2]lili",
                     vec![
-                        ModuleItem::new_question("Q1", "[Q1]lala\n"),
+                        ModuleItem::new_question("Q1", "lala\n"),
                         ModuleItem::new_question("Q2", "lili")
                     ]
                 )
@@ -367,5 +411,133 @@ mod tests {
         let re = Regex::new(r"//.*").unwrap();
         let clean_text = re.replace_all(text, "");
         println!("{}", clean_text);
+    }
+
+    #[test]
+    fn test_comment_in_preamble() {
+        let input = "// this is a comment with a question header [Q1]\n[Q1] ding dong";
+        let (input, preamble) = take_until_next_module_item(input).unwrap();
+        assert_eq!(
+            preamble,
+            "// this is a comment with a question header [Q1]\n"
+        );
+        assert_eq!(input, "[Q1] ding dong");
+
+        let input = "[Q1] ding dong";
+        let (input, preamble) = take_until_next_module_item(input).unwrap();
+        assert_eq!(input, "[Q1] ding dong");
+        assert_eq!(preamble, "");
+
+        let input = "[Q1] ding dong";
+        let (input, preamble) = take_until_next_module_item(input).unwrap();
+        assert_eq!(input, "[Q1] ding dong");
+        assert_eq!(preamble, "");
+    }
+
+    #[test]
+    fn test_parse_whitespace_or_comment() {
+        let input = "// this is a comment with a question header [Q1]\n[Q1] ding dong";
+        let (input, _) = parse_whitespace_or_comment(input).unwrap();
+        println!("{}", input);
+        assert_eq!(input, "[Q1] ding dong");
+    }
+
+    #[test]
+    fn test_d1() {
+        let input = r#"       
+        [Q1] Question
+        [1] response 1
+        [2] response 2
+        [3] response 3
+
+        [Q2 displayif="lala"] Another Question
+        [1] r1
+        [3] r3
+        [4] r4
+        "#;
+
+        fn parse_p1(input: &str) -> IResult<&str, &str> {
+            let (input, _) = multispace0(input)?;
+            println!("input:\n{}", input);
+            let (input, q) = delimited(tag("["), take_until("]"), tag("]"))(input)?;
+            Ok((input, q))
+        }
+
+        let res = parse_p1(input);
+        match res {
+            Ok((input, q)) => {
+                println!("new input\n{}\nq:{}", input, q);
+            }
+            Err(e) => {
+                eprintln!("{:#?}", e);
+                assert!(false)
+            }
+        }
+    }
+
+    #[test]
+    fn peeking() {
+        let input = r#"
+        // this is a comment
+        [INTROM]Hello and welcome to the survey,  It is a prelude of what 
+        you will see... 
+        [Q1] Question
+        [1] response 1
+        [2] response 2
+        [3] response 3
+
+        [Q2 displayif="lala"] Another Question
+        [1] r1
+        [3] r3
+        [4] r4
+        "#;
+
+        fn q_parser(input: &str) -> IResult<&str, &str> {
+            println!(" The input string slice is {} chars long", input.len());
+            println!(" ===> Question Parser called on {}", input);
+            if input.len() < 2 {
+                return Err(Err::Error(Error::new(
+                    "not long enough to be a question",
+                    nom::error::ErrorKind::Fail,
+                )));
+            }
+            let mut iter = input.char_indices();
+            let mut end_index = input.len();
+            while let Some((indx, _)) = iter.next() {
+                if indx == 0 {
+                    continue;
+                }
+                // check if we are at a '[X'
+                let mut iter2 = input.chars();
+                let r1 = iter2.nth(indx);
+                let r2 = iter2.next();
+                if let (Some(x), Some(y)) = (r1, r2) {
+                    if x == '[' && y.is_uppercase() {
+                        end_index = indx;
+                        break;
+                    }
+                }
+            }
+            // Dont return string slice return a QuestionType
+            Ok((&input[end_index..], &input[0..end_index]))
+        }
+        fn parse_m(input: &str) -> IResult<&str, Vec<&str>> {
+            let (input, (_, q)) = tuple((multispace0, many1(q_parser)))(input)?;
+            Ok((input, q))
+        }
+        let res = parse_m(input);
+        println!();
+        match res {
+            Ok((input, qs)) => {
+                for q in qs {
+                    println!("q:\n{}", q);
+                }
+                println!("new input:\n{}", input);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false)
+            }
+        }
     }
 }
